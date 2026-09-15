a5b04eaa1d0b9996e354cb6ba921b4ebb373167a
sarah <sasahackermans@gmail.com>
2026-09-15 08:17:17 -0300

    Create SQL script for projeto_backend_abdias database

---
+++ BD/script.sql
@@ -0,0 +1,12 @@
+--
+CREATE DATABASE projeto_backend_abdias;
+--
+USE projeto_backend_abdias;
+--
+CREATE TABLE login (
+    id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    login VARCHAR(50),
+    senha VARCHAR(10)
+);
+--
